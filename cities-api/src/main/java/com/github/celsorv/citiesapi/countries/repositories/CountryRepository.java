8083c409bc196ffe5ec4dfdc7b64544509c5e2e9
Celso Vitorino <vitorino.celso@gmail.com>
2022-10-11 10:32:52 -0300

    Delete cities-api directory

--- cities-api/src/main/java/com/github/celsorv/citiesapi/countries/repositories/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.celsorv.citiesapi.countries.repositories;
-
-import com.github.celsorv.citiesapi.countries.entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
--- cities-api/src/main/java/com/github/celsorv/citiesapi/states/repositories/StateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.celsorv.citiesapi.states.repositories;
-
-import com.github.celsorv.citiesapi.states.entities.State;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
